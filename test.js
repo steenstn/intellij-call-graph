034e8969c72e41029d2afe16195d5a4726e2449e
Alexander Steen <alexander.steen@mynt.se>
2025-07-08 22:28:06 +0200

    Refactor test

--- test.js
+++ test.js
@@ -3,16 +3,18 @@ let assertEquals = (actual, expected) => {
         throw new Error("Expected:\n" + expected + "\n\nActual\n" + actual);
     }
 }
+
 let test = (functionToTest, input) => {
-    let i = 0;
+    let i;
     for(i = 0; i < input.length; i+=2) {
         let output = functionToTest(input[i])
         assertEquals(output, input[i+1])
     }
+
+    document.getElementById("test_results").innerText +=  "\n" + i + " tests passed";
 }
 
 test(replaceLine, replaceLineTestCases);
-document.getElementById("test_results").innerText =  "replaceLine tests passed\n";
-
 test(parseCallGraph, fullTestCases);
-document.getElementById("test_results").innerText +=  "parseCallGraph tests passed";
+
+document.getElementById("test_results").innerText +=  "\nAll test passed" ;
